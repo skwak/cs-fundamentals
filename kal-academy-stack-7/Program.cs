1b46fd4aa4bcc67f16ad8d4175c0a11e65c998da
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-10-19 21:40:22 -0700

    set up 7th stack question

---
+++ kal-academy-stack-7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace kalacademystack7
+{
+	class MainClass
+	{
+
+		public static void Main (string[] args)
+		{
+			//7. Evaluate an expression given only single digits and only 2 operators * and +.
+
+			string expression = "2 + 5 * 6 + 1";
+
+			string[] chars = expression.Split (' ');
+
+			Console.WriteLine (chars);
+
+			foreach(string s in chars) Console.WriteLine(s);
+
+			var stack = new Stack<int> ();
+
+		}
+	}
+}
