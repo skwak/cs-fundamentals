1551eefe434e72ba820b650c22d9b78f7ed4f9d9
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-10-25 10:28:36 -0700

    add some new problems, to be completed

---
+++ kal-academy-number-to-string/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace kalacademynumbertostring
+{
+	class MainClass
+	{
+		//Given any integer, print an English phrase that describes the integer (e.g., “One Thousand, Two Hundred Thirty Four”)
+		public static void Main (string[] args)
+		{
+
+			string stringyNum = "One Thousand, Two Hundred Thirty Four";
+
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
